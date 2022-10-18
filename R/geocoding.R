c3493d3cbcb42cfc1106105391eb7013a8df9711
EivydasKoc <e.kocinas@gmail.com>
2022-10-18 13:36:32 +0300

    add bounds

--- R/geocoding.R
+++ R/geocoding.R
@@ -8,6 +8,9 @@
 #' @param within.country Only return the results that are within the specified country.
 #'  If no results are found it will return the country itself. Optional. Format:ISO 3166-1 alpha-2 or alpha-3
 #' @param exclude.location.types Exclude location types from results. Available values: "country". Optional.
+#' @param bounds Used to limit the results to a bounding box. Expecting a character vector with four floats,
+#' marking a south-east and north-west corners of a rectangle: min-latitude,min-longitude,max-latitude,max-longitude.
+#' e.g. bounds for Scandinavia c(54.16243,4.04297,71.18316,31.81641). Optional.
 #'
 #' @return API response parsed as list and as a raw json
 #' @export
@@ -15,11 +18,12 @@
 #' @examples \dontrun{
 #' geocoding('Parliament square')
 #' }
-geocoding <- function(query, within.country = NA, exclude.location.types = NA) {
+geocoding <- function(query, within.country = NA, exclude.location.types = NA, bounds = NA) {
 
   queryFull <- c(query = query,
                  within.country = within.country,
-                 exclude.location.types = exclude.location.types)
+                 exclude.location.types = exclude.location.types,
+                 bounds = paste(as.character(bounds), collapse=",")[!missing(bounds)])
 
   traveltime_api(path = c('geocoding', 'search'), query = as.list(queryFull)[!is.na(queryFull)])
 }
