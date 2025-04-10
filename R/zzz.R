79de96c92004cd43009a5cbcbe3c0e9ffa83bffa
arnasbr <arnas.bradauskas@traveltime.com>
2025-04-10 18:15:11 +0300

    remove unused line

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,8 @@
 .onLoad <- function(libname, pkgname) {
   f <- system.file("proto", package = "traveltimeR")
 
-  # Get all proto files except RequestsCommon.proto to avoid duplicate definitions (will crash otherwise)
+  # Get all proto files except RequestsCommon.proto to avoid duplicate definitions
   protoFiles <- list.files(f, pattern = "\\.proto$", full.names = TRUE)
-  commonProtoPath <- file.path(f, "RequestsCommon.proto")
   protoFiles <- protoFiles[!grepl("RequestsCommon\\.proto$", protoFiles)]
 
   # Read only these files - they'll import RequestsCommon.proto as needed
