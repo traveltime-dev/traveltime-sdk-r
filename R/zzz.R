7c62e99faa18ed71ef0e56f30a2875f405632c0a
arnasbr <arnas.bradauskas@traveltime.com>
2025-04-10 17:04:41 +0300

    new proto files + modified parsing

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,11 @@
 .onLoad <- function(libname, pkgname) {
   f <- system.file("proto", package = "traveltimeR")
-  RProtoBuf::readProtoFiles(dir = f)
+
+  # Get all proto files except RequestsCommon.proto to avoid duplicate definitions (will crash otherwise)
+  protoFiles <- list.files(f, pattern = "\\.proto$", full.names = TRUE)
+  commonProtoPath <- file.path(f, "RequestsCommon.proto")
+  protoFiles <- protoFiles[!grepl("RequestsCommon\\.proto$", protoFiles)]
+
+  # Read only these files - they'll import RequestsCommon.proto as needed
+  RProtoBuf::readProtoFiles(protoFiles)
 }
